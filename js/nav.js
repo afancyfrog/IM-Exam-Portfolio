07c0c03df1677228c6970b3ef0453fef905d1078
afancyfrog <tanyafreese@gmail.com>
2026-06-10 16:34:57 +0200

    i think this is right?

--- js/nav.js
+++ js/nav.js
@@ -1,41 +1,14 @@
-const BASE = "/IM-Exam-Portfolio";
-
 async function loadNavbar() {
-  const response = await fetch(`${BASE}/components/navbar.html`);
+  const response = await fetch("components/navbar.html");
   const html = await response.text();
 
   document.getElementById("navbar").innerHTML = html;
 
-  normalizeNavbarLinks();
-  fixNavbarAssets();
-
   highlightCurrentPage();
   setupHamburger();
-  initScrollLinks();
-  handlePageHashScroll();
 }
 
 loadNavbar();
-function normalizeNavbarLinks() {
-  document.querySelectorAll(".nav-links a").forEach((link) => {
-    let href = link.getAttribute("href");
-    if (!href) return;
-
-    if (href.startsWith(BASE)) return;
-
-    if (href.startsWith("/")) {
-      link.href = BASE + href;
-    }
-  });
-}
-
-function fixNavbarAssets() {
-  const logo = document.querySelector(".logo img");
-  const hamburger = document.querySelector(".hamburger img");
-
-  if (logo) logo.src = `${BASE}/assets/LowResLogo3.png`;
-  if (hamburger) hamburger.src = `${BASE}/assets/HamburgerIcon2.png`;
-}
 
 function highlightCurrentPage() {
   const current = window.location.pathname;
